3aebef49eb5bd9a1d60da09143f1c6961c5f8eac
Dev-Myles <dev-myles@Debian-Linux>
2022-11-18 15:37:59 -0600

    fix: issues with the userData schema

--- src/schema/userData.ts
+++ src/schema/userData.ts
@@ -4,7 +4,7 @@ export const userDataSchema = z.object({
   firstName: z.string().max(32).min(2),
   lastName: z.string().max(32).min(2),
   email: z.string().email(),
-  desiredEmail: z.string().email(),
+  desiredEmail: z.string(),
 });
 
 export type UserDataSchema = z.TypeOf<typeof userDataSchema>;
